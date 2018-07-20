33aec62e0f48043f14a88d1b7ce938834061b335
EC2 Default User <ec2-user@ip-172-31-39-140.us-east-2.compute.internal>
2018-07-20 16:57:37 +0000

    test

--- application-controller.rb
+++ application-controller.rb
@@ -1,14 +1,56 @@
 require 'bundler'
 Bundler.require
-#require_relative 'models/questions'
+require_relative 'models/questions'
 
 class MyApp < Sinatra::Base
 
  get'/' do
   erb :index
  end
-
  
+get '/index1.erb' do
+    erb :index1
+  end
+  
+  get '/action_page.php' do
+    erb :index
+  end
+  get '/signup.erb' do 
+   erb :signup
+  end 
+  
+  get '/index.erb' do
+    "Hello World"
+  end
+ get '/questions' do
+   erb :questions
+ end
 
- 
+
+
+ post  '/results' do
+   answers = params.values
+   @total= 0
+   answers.each do |answer|
+     @total += answer.to_i
+   end
+   puts @total
+
+   @workout = quiz(@total)
+   if @workout == "low cardio"
+     erb :lowcardio
+   elsif @workout == "medium cardio"
+     erb :mediumcardio
+   elsif @workout == "hard cardio"
+     erb :hardcardio
+     elsif @workout == "low strength"
+     erb :lowstrength
+     elsif @workout == "medium strength"
+     erb :mediumstrength
+     elsif @workout == "hard strength"
+     erb :hardstrength
+  end
 end
+end
+ 
+
--- models/questions.rb
+++ models/questions.rb
@@ -1,9 +1,16 @@
-# def cookie_chooser(user_value)
-#  if user_value <4
-#     "cranberry"
-#  elsif user_value <7
-#     "trailmix"
-#  elsif user_value <10
-#     "chocolatey"
-# end
-# end
+def quiz(user_value)
+if user_value <4
+   "low cardio"
+elsif user_value <6
+   "medium cardio"
+elsif user_value <8
+   "hard cardio"
+elsif user_value <103
+   "low strength"
+elsif user_value <105
+   "medium strength"
+elsif user_value <107
+   "hard strength"
+   
+end
+end
